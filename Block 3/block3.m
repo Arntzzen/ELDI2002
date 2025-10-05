df8613415d91099f331032d3ec7bc057fab75d01
Arntzzen <markus.arntzen@gmail.com>
2025-10-05 18:45:40 +0200

    Lagt til kode for å plotte flere verdier for a og gamma i transfer-funksjonen i warm up task 2.

--- Block 3/block3.m
+++ Block 3/block3.m
@@ -33,26 +33,39 @@ riseTime = stepinfo(G).RiseTime;
 
 % Warm up task 2:
 
-PO = 5;                                                    % Percent overshoot
-Tp = 0.002;                                                 % Peak time
-
-z = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2));        % Calculated formula for zeta
-wn = pi / (Tp*sqrt(1-z^2));                                 % Calculated formula for wn
-balance = 10*z*wn;
+PO = 5;                                                      % Percent overshoot
+Tp = 0.002;                                                  % Peak time
+    
+z = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2));         % Calculated formula for zeta
+wn = pi / (Tp*sqrt(1-z^2));                                      % Calculated formula for wn
+balance = 10*z*wn
 
 gam = 5000;
-a = 5000;
+%a1 = 5000;
 
-s = tf('s');
-G = ((gam/a)*(s+a)*(wn^2)) / ((s^2 + 2*z*wn*s + wn^2)*(s+gam));   % Transfer function from warm up task 1
+%s = tf('s');
+%G1 = ((gam1/a1)*(s+a1)*(wn^2)) / ((s^2 + 2*z*wn*s + wn^2)*(s+gam1));   % Transfer function from warm up task 1
 
-t = 0:0.000001:0.01;                                       % Time vector
-y = step(G, t);                                             % Function generating step response for the transfer function "G" and time interval "t"
+t = 0:0.000001:0.01;                                             % Time vector
+%y = step(G, t);                                                 % Function generating step response for the transfer function "G" and time interval "t"
+
+    a_val = [5000, 1000, 500];
 
 
-% Plot the step response
+
+% Plot of the step response
 figure;
-plot(t, y);
+hold on;
+for i = 1:length(a_val)
+    a = a_val(i);
+    s = tf('s');
+    G = ((gam/a)*(s+a)*(wn^2)) / ((s^2 + 2*z*wn*s + wn^2)*(s+gam));
+    [y, ~] = step(G, t);
+    plot(t, y, 'DisplayName', ['a = ' num2str(a)]);
+end
+
+hold off;
+legend show;
 xlabel('Time (s)');
 ylabel('Response');
 title('Step Response of the System');
@@ -61,5 +74,5 @@ grid on;
 % Calculate the settling time and rise time
 format long
 settlingTime = stepinfo(G).SettlingTime
-riseTime = stepinfo(G).RiseTime;
+riseTime = stepinfo(G).RiseTime
 settlingValue = y(end)
