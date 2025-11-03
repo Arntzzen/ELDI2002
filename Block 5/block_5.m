d0c099febdcf98cd91b190c6a7adcb7d9209ca92
Arntzzen <markus.arntzen@gmail.com>
2025-11-03 22:57:02 +0100

    Alt er greit

--- Block 5/block_5.m
+++ Block 5/block_5.m
@@ -3,8 +3,8 @@
 PO = 10;
 Ts = 0.002;
 
-zeta = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2))
-wn = 1 / (zeta * Ts)
+zeta = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2));
+wn = 1 / (zeta * Ts);
 
 gamma = 7788.62;
 Kp = 0.0087;
