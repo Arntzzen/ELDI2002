ffdf32bb4a89e3386406f6b6cd690427a307e89d
Arntzzen <markus.arntzen@gmail.com>
2025-11-03 17:28:13 +0100

    Lagt til variabler; kalkulert gamma, Kp og Ki.

--- Block 5/block_5.m
+++ Block 5/block_5.m
@@ -1,7 +1,12 @@
 %% Block 5
 
+%% Defining variables
 PO = 10;
 Ts = 0.002;
 
-zeta = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2))
-wn = 1 / (zeta * Ts)
+zeta = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2));
+wn = 1 / (zeta * Ts);
+
+gamma = -7788.62;       % Mulig feil fortegn?!?
+Kp = 0.0087;
+Ki = gamma * Kp;
