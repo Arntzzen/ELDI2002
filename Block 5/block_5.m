43d3045d11255dda1cc1ea253e63540c9dc8b609
Arntzen <markus.arntzen@gmail.com>
2025-11-06 17:31:00 +0100

    Lagt til beregninger for PI-controller og plot for root locus med tilhørende visning av desired poles.

--- Block 5/block_5.m
+++ Block 5/block_5.m
@@ -1,36 +1,74 @@
 %% Block 5
-
+%%%%%%%%%%% Defining values and criterias%%%%%%%%%%%%
 PO = 10;
 Ts = 0.002;
 
 zeta = (-log(PO/100)) / (sqrt(pi^2 + (log(PO/100))^2));
 wn = 1 / (zeta * Ts);
 
-gamma = 7788.62;
-Kp = 0.0087;
-Ki = gamma * Kp;
-
-
+% These are values from our second order tf assigned to variables
 s1 = -499 + 682.9j;
 a = 1549508692 / 158730;
 co = 158730;
 b = 602.5;
 d = 126615937;
 
-GclPI = co * ((s1 + a) / (s1 * ((s1)^2 + b*s1 + d)));
+% This is our loop tf with PI, but only with the s at origin and not
+% the term  with gamma. We will calculate that later.
+GolPI = co * ((s1 + a) / (s1 * ((s1)^2 + b*s1 + d))*(s1+20))
 
 
-%%%%%%%%% Chat %%%%%%%%%%
+%%%%%%%%% Angle criterion to find gamma %%%%%%%%%%
+% Define desired pole sd
+sd = -499+682.9j;
 
-% Finn total vinkel (i grader)
-phi_total = angle(GclPI) * 180/pi;
-%disp(phi_total)
+% calculating numeric values of each term (again, without gamma-term).
+v1_num = sd + a
+v2_num = sd;
+v3_num = sd + (-301.3+11264.4j);
+v4_num = sd + (-301.3-11264.4j);
 
-phi_hoyre_side_av_likhetstegnet = -180 - phi_total;
+% Finding angles in degrees of each term.
+v1_ang = angle(v1_num) * (180/pi);
+v2_ang = angle(v2_num) * (180/pi);
+v3_ang = angle(v3_num) * (180/pi);
+v4_ang = angle(v4_num) * (180/pi);
 
-hoyreside = tan(phi_hoyre_side_av_likhetstegnet)
+% Calculating the sum of each angle and moving over to the right side
+% of the equation and then getting rid of the arctan from the left side.
+anglesum = v1_ang + v2_ang + v3_ang + v4_ang
+rightside = -180 - anglesum + 360
+rightside_tan = tan(rightside * (pi/180))
 
-
+% Solving for gamma.
 syms gam
-losning = 682.9 / (-499 + gam) == hoyreside;
-l = double(solve(losning))
+eq = 682.9 / (-499+gam) == rightside_tan
+gamma = double(solve(eq))
+
+
+
+%%%%%%%%% Magnitude criterion to find Kp and Ki %%%%%%%%%%
+
+% Calculate the magnitude of the loop transfer function
+magnitude = abs(GolPI);
+% Set the desired magnitude for Kp
+rightside = 1;
+Kp = rightside / magnitude
+Ki = gamma * Kp
+
+
+%%%%%%%%%%%%%%% Plot root locus of Closed Loop PI %%%%%%%%%%%
+
+desired_pole = -499 + 682.9j
+s = tf('s');
+Gol = ((Kp*(s+gamma)) / s) * ((s+a) / ((s^2 + b*s + d)*(s+20)))
+
+figure;
+rlocus(Gol);
+hold on;
+plot(desired_pole, 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+xlabel('Re(s)');
+ylabel('Im(s)');
+title('Root locus with desired poles');
+grid on;
+legend('Root Locus', 'Desired poles');
