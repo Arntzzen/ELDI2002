b4d0679699cb4b31ec415937e4d51f026d47e092
Arntzzen <markus.arntzen@gmail.com>
2025-11-22 23:31:03 +0100

    Kjørt simuleringer

--- Block 6/block_6.m
+++ Block 6/block_6.m
@@ -21,11 +21,11 @@ syms k1 k2 lambda
 k = [k1 k2];
 system = A-B*k
 eq = charpoly(system, lambda)
-sol = solve(eq, k);
-[coef, powers] = coeffs(eq, lambda);
-collected = arrayfun(@(c) collect(c, [k1 k2]), coef);
-final = sum (collected .* lambda.^powers);
-terms = collected .* lambda.^powers;
+sol = solve(eq, k)
+[coef, powers] = coeffs(eq, lambda)
+collected = arrayfun(@(c) collect(c, [k1 k2]), coef)
+final = sum (collected .* lambda.^powers)
+terms = collected .* lambda.^powers
 %arrayfun(@(t) pretty(t), terms);
 pretty(terms(2))
 
@@ -40,8 +40,8 @@ eq1 = first_term == two_zeta_wn;
 eq2 = second_term == wn_square;
 
 sol_k = solve([eq1, eq2], [k1, k2])
-k1 = double(sol_k.k1);
-k2 = double(sol_k.k2);
+k1 = double(sol_k.k1)
+k2 = double(sol_k.k2)
 k = [k1, k2];
 %{
 k = [sol.k1, sol.k2]
@@ -54,18 +54,18 @@ system = A-B*k;
 sys_cl = ss(system, Bm, Cm, 0)
 eig(sys_cl)
 
-
+%{
 figure;
 step(sys_cl);
 grid on;
 title('Step response of closed-loop system');
-
+%}
 Cekte = ctrb(A,B)
 observable = obsv(A,Cekte)
 s
 %% LQR-controller
 % Define the Q and R matrices for LQR
-Q = [0.000001 0; 0 10000000]           % State weighting matrix
+Q = [1 0; 0 1]           % State weighting matrix
 Rm = 1;                      % Control weighting scalar
 
 
@@ -77,11 +77,11 @@ Acl_lqr = A-B*K;
 sys_lqr = ss(Acl_lqr, B, Cm, 0);
 eig(Acl_lqr)
 
-
+%{
 figure;
 step(sys_lqr);
 grid on;
 title('Step Response – LQR Controlled System');
-
+%}
 stepinfo(sys_cl)
 stepinfo(sys_lqr)
